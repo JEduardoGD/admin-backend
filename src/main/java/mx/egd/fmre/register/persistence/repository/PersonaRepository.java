4617ffab36d39d0620635d8bbc8509d7f6ae94d0
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2026-08-19 22:30:34 -0600

    avance

--- src/main/java/mx/egd/fmre/register/persistence/repository/PersonaRepository.java
+++ src/main/java/mx/egd/fmre/register/persistence/repository/PersonaRepository.java
@@ -3,10 +3,12 @@ package mx.egd.fmre.register.persistence.repository;
 import java.util.Date;
 import java.util.List;
 
+import org.springdoc.core.converters.models.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import mx.egd.fmre.register.dto.datatable.QueryObj;
 import mx.egd.fmre.register.persistence.entity.PersonaEntity;
 
 @Repository
@@ -29,4 +31,20 @@ public interface PersonaRepository extends JpaRepository<PersonaEntity, Integer>
             UPPER(p.primerApellido) = UPPER(?2)
              """)
     List<PersonaEntity> findByNombreAndPrimerApellido(String nombre, String primerApellido);
+    
+    @Query("""
+            SELECT p FROM PersonaEntity p WHERE
+                 p.nombre LIKE '%:nombre%' OR
+                 p.primerApellido LIKE '%:primerApellido%' OR
+                 p.segundoApellido LIKE '%:segundoApellido%' OR
+            """)
+    List<PersonaEntity> searchByConditions(Pageable pageable);
+    
+    @Query("""
+            SELECT p FROM PersonaEntity p WHERE
+                 p.nombre REGEXP :nombre OR
+                 p.primerApellido REGEXP :primerApellido OR
+                 p.segundoApellido REGEXP :segundoApellido OR
+            """)
+    List<PersonaEntity> searchByRegexConditions(QueryObj queryObj, Pageable pageable);
 }
--- src/main/java/mx/egd/fmre/register/service/impl/DatatableServiceImpl.java
+++ src/main/java/mx/egd/fmre/register/service/impl/DatatableServiceImpl.java
@@ -3,12 +3,17 @@ package mx.egd.fmre.register.service.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import mx.egd.fmre.register.dto.datatable.DataTableResponse;
 import mx.egd.fmre.register.dto.datatable.DatatableObj;
 import mx.egd.fmre.register.dto.datatable.QueryObj;
+import mx.egd.fmre.register.dto.datatable.QueryObj.Column;
+import mx.egd.fmre.register.dto.datatable.QueryObj.Search;
 import mx.egd.fmre.register.persistence.entity.PersonaEntity;
 import mx.egd.fmre.register.persistence.repository.PersonaRepository;
 import mx.egd.fmre.register.service.DatatableService;
@@ -19,14 +24,20 @@ public class DatatableServiceImpl implements DatatableService {
     
     private final PersonaRepository personaRepository;
     
+    private static final String REGEXP = "REGEXP";
+    private static final String LIKE = "LIKE";
+    private static final String SPACE = " ";
+    private static final String S_OR_S = " OR ";
+    
     @Override
     public DataTableResponse get(QueryObj queryObj) {
-        List<PersonaEntity> personaList = personaRepository.findAll();
+        String searchConditions = createSearchConditions(queryObj);
+        Pageable pageable = PageRequest.of(1, 10, Sort.by("registrationDate").descending());
+        List<PersonaEntity> personaList = personaRepository.searchByRegexConditions(queryObj, pageable);
         return processList(queryObj, personaList);
     }
     
     private DataTableResponse processList(QueryObj queryObj, List<PersonaEntity> personaList) {
-        int draw = 1;
         long recordsTotal = personaList.stream().count();
         long recordsFiltered = personaList.stream().count();
         List<DatatableObj> data = personaList.stream().map(p -> {
@@ -43,11 +54,28 @@ public class DatatableServiceImpl implements DatatableService {
         }).collect(Collectors.toList());
 
         DataTableResponse dataTableResponse = new DataTableResponse();
-        dataTableResponse.setDraw(draw);
+        dataTableResponse.setDraw(queryObj.draw());
         dataTableResponse.setRecordsTotal(recordsTotal);
         dataTableResponse.setRecordsFiltered(recordsFiltered);
         dataTableResponse.setData(data);
 
         return dataTableResponse;
+    }
+    
+    private String createSearchConditions(QueryObj queryObj) {
+        Search search = queryObj.search();
+        List<Column> searchableColumns = queryObj.columns().stream()
+                .filter(Column::searchable)
+                .toList();
+        return searchableColumns.stream().map(c -> {
+            StringBuffer strSearchColumn = new StringBuffer();
+            strSearchColumn
+                    .append(c.data())
+                    .append(SPACE)
+                    .append(search.regex() ? REGEXP : LIKE)
+                    .append(SPACE)
+                    .append(String.format("'%%%s%%'", search.value()));
+            return strSearchColumn.toString();
+        }).collect(Collectors.joining(S_OR_S));
     }
 }
